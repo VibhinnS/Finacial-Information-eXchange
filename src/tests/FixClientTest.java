91592e0e7387ec21856eb09c37dfcf730e438999
VibhinnS <vibhinnsinghal25@gmail.com>
2024-09-12 14:09:25 +0530

    Fixed pom.xml added correct dependency tags All code compiled

--- src/tests/FixClientTest.java
+++ src/tests/FixClientTest.java
@@ -1,6 +1,9 @@
 package fix.client;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class FixClientTest {
@@ -23,7 +26,6 @@ public class FixClientTest {
                     System.out.println("Received response: " + response);
 
                 } catch (IOException e) {
-                    e.printStackTrace();
                 }
             }).start();
         }
